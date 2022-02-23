3c2ed464c4a88b3be796f9d03d104d4f0d7d2c0a
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2022-02-24 00:53:10 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+//Making Footer date automatic update
+
+var date = new Date()
+var curentYear = date.getFullYear()
+document.getElementById("current_footer_date").innerHTML = curentYear
+
+
+var x = ["John", "Albert", "Sue", "Frank", "Bob"];
+
+var r = new RegExp("js");
+console.log(r)
